9aa1718aec3f7dd76dc908ff573b3aa8fe46aec4
Matthew Wolstenholme <matt.t.w7@gmail.com>
2019-06-13 15:37:35 +1000

    Update postinstall.cmd
    add node-red-contrib-config

--- postinstall.cmd
+++ postinstall.cmd
@@ -3,7 +3,7 @@
 (if not exist \home\nodered\.node-red mkdir \home\nodered\.node-red)
 copy npm_.cmd \home\nodered\.node-red\npm_.cmd
 pushd \home\nodered\.node-red
-npm install node-red-dashboard node-red-contrib-ui_list node-red-contrib-azureiotc2d git://github.com/wolsty7/NodeRedIoTHub.git git://github.com/wolsty7/NodeRed-Azure-SQL.git & (
+npm install node-red-dashboard node-red-contrib-ui_list node-red-contrib-azureiotc2d git://github.com/wolsty7/NodeRedIoTHub.git git://github.com/wolsty7/NodeRed-Azure-SQL.git node-red-contrib-config & (
      copy npm_.cmd npm.cmd /y
      popd 
      copy npm_.cmd npm.cmd /y
