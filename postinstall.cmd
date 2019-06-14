5ad90c695b4542ab8be2768868e47d9c55eebc69
Matthew Wolstenholme <matt.t.w7@gmail.com>
2019-06-14 12:04:02 +1000

    fix directory

--- postinstall.cmd
+++ postinstall.cmd
@@ -1,8 +1,9 @@
 (if not exist \home mkdir \home)
-(if not exist \home\nodered mkdir \home\nodered)
-(if not exist \home\nodered\.node-red mkdir \home\nodered\.node-red)
-copy npm_.cmd \home\nodered\.node-red\npm_.cmd
-pushd \home\nodered\.node-red
+(if not exist \home\site mkdir \home\site)
+(if not exist \home\site\nodered mkdir \home\site\nodered)
+(if not exist \home\site\nodered\.node-red mkdir \home\site\nodered\.node-red)
+copy npm_.cmd \home\site\nodered\.node-red\npm_.cmd
+pushd \home\site\nodered\.node-red
 npm install node-red-dashboard node-red-contrib-ui_list node-red-contrib-azureiotc2d git://github.com/wolsty7/NodeRedIoTHub.git git://github.com/wolsty7/NodeRed-Azure-SQL.git node-red-contrib-md5 & (
      copy npm_.cmd npm.cmd /y
      popd 
